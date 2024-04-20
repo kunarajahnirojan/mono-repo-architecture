3589d866f9d1290c73a05c8aa07d6ec3497ec0af
Nirojan <kunarajahnirojan@gmail.com>
2024-04-20 16:26:46 +0530

    router modifications

--- libs/router/src/index.ts
+++ libs/router/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/customer';
+export * from './lib/admin';
---
+++ libs/router/src/lib/admin/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Lazy, NotFound } from '@mono-repo-architecture/components/base';
+import { CustomerLayout } from '@mono-repo-architecture/layouts';
+import { HOME, ABOUT } from '@mono-repo-architecture/constants';
+import { Home, About } from '@mono-repo-architecture/customer/feature';
+
+export const AdminRouter = () => {
+  return (
+    <Routes>
+      <Route path={HOME} element={<CustomerLayout />}>
+        <Route index element={<Lazy page={Home} />} />
+
+        <Route path={HOME} element={<Lazy page={About} />} />
+        <Route path={ABOUT} element={<Lazy page={About} />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
